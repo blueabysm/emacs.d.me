37d46c240f6b0e437365114b32f91b1f828f275c
pluto <blueabysm@gmail.com>
2012-07-03 22:03:48 +0800

    customize color face

--- custom.el
+++ custom.el
@@ -15,3 +15,14 @@
 (setq-default c-basic-offset 4)
 (setq ruby-indent-level 4)
 (setq-default tab-width 4)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face))))
+ '(font-lock-comment-face ((t (:foreground "blue"))))
+ '(font-lock-constant-face ((t (:foreground "brightred"))))
+ '(font-lock-function-name-face ((t (:foreground "brightblue"))))
+ '(font-lock-keyword-face ((t (:foreground "brightyellow"))))
+ '(font-lock-string-face ((t (:foreground "brightgreen")))))
