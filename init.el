f9789653dc233dd8e4178b432d12d9bc01f0cfb6
pluto <blueabysm@gmail.com>
2012-07-03 22:07:36 +0800

    delete comments of init.el

--- init.el
+++ init.el
@@ -6,20 +6,15 @@
 ;; custom.el
 (setq custom-file "~/.emacs.d/custom.el")
 
-;; init packages
 (require 'init-site-lisp)
 (require 'init-elpa)
 (require 'init-ido)
 (require 'init-auto-complete)
-
+(require 'init-uniquify)
 ;; stand alone key bindings
 (require 'init-keybindings)
-
-;; modes
 (require 'init-php)
 (require 'init-sql)
-
-;; miscs
 (require 'init-misc)
 (require 'init-look)
 
