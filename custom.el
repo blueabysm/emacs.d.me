815c02fbb8799bf4684bff2a854f0c93618ba2da
pluto <blueabysm@gmail.com>
2012-07-30 11:43:01 +0800

    customize flymake-errline face

--- custom.el
+++ custom.el
@@ -20,6 +20,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(flymake-errline ((t (:background "blue" :foreground "brightwhite"))))
  '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face))))
  '(font-lock-comment-face ((t (:foreground "blue"))))
  '(font-lock-constant-face ((t (:foreground "brightred"))))
