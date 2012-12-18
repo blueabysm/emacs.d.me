46e4cfb41a473d1a49d2b40049bb68f8eac0fea0
pluto <blueabysm@gmail.com>
2012-12-18 11:52:09 +0800

    backup

--- custom.el
+++ custom.el
@@ -22,7 +22,7 @@
  ;; If there is more than one, they won't work right.
  '(flymake-errline ((t (:background "blue" :foreground "brightwhite"))))
  '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face))))
- '(font-lock-comment-face ((t (:foreground "blue"))))
+ '(font-lock-comment-face ((t (:foreground "#00AADD"))))
  '(font-lock-constant-face ((t (:foreground "brightred"))))
  '(font-lock-doc-face ((t (:inherit font-lock-string-face :foreground "blue"))))
  '(font-lock-function-name-face ((t (:foreground "brightblue"))))
@@ -41,3 +41,6 @@
 
 ;; key-bindings
 (global-set-key (kbd "C-/") 'undo)
+
+;; truncate line
+(toggle-truncate-lines)
